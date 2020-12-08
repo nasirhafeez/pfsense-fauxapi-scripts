dee116dc805be550bcaa4c048db318699705dade
Nasir Hafeez <nasirhafeez2002@gmail.com>
2020-12-08 16:49:32 +0500

    get interface stats

---
+++ get-interface-stats.py
@@ -0,0 +1,6 @@
+import pprint, sys
+from PfsenseFauxapi.PfsenseFauxapi import PfsenseFauxapi
+PfsenseFauxapi = PfsenseFauxapi('192.168.100.54', 'PFFAudv9Chfn2n7W1us5utjH', '9XjKvnBRdNDCviXr9bZsTzKkIl4rZwp06gs0ImW4vV6kE1k16ibJ4SyPLJcz')
+
+stats = PfsenseFauxapi.interface_stats('em0')
+pprint.pprint(stats)
