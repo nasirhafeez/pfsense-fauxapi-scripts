c0c98ff4d521dcb4ee0b7ba2dd9188c98ef5870c
Nasir Hafeez <nasirhafeez2002@gmail.com>
2020-12-08 17:00:10 +0500

    dotenv added

--- get-interface-stats.py
+++ get-interface-stats.py
@@ -1,6 +1,15 @@
-import pprint, sys
+import pprint, sys, os
 from PfsenseFauxapi.PfsenseFauxapi import PfsenseFauxapi
-PfsenseFauxapi = PfsenseFauxapi('192.168.100.54', 'PFFAudv9Chfn2n7W1us5utjH', '9XjKvnBRdNDCviXr9bZsTzKkIl4rZwp06gs0ImW4vV6kE1k16ibJ4SyPLJcz')
+from dotenv import load_dotenv
 
+load_dotenv()
+
+hostip = os.getenv("HOST_IP")
+apikey = os.getenv("API_KEY")
+apisecret = os.getenv("API_SECRET")
+
+########## Script starts here ##########
+
+PfsenseFauxapi = PfsenseFauxapi(hostip, apikey, apisecret)
 stats = PfsenseFauxapi.interface_stats('em0')
 pprint.pprint(stats)
