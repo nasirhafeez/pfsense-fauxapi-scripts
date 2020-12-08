99eb7787557e2eb85d5ff0197640116266f487c5
Nasir Hafeez <nasirhafeez2002@gmail.com>
2020-12-08 18:28:01 +0500

    rename

---
+++ users-get.py
@@ -0,0 +1,21 @@
+import pprint, sys, os
+from PfsenseFauxapi.PfsenseFauxapi import PfsenseFauxapi
+from dotenv import load_dotenv
+
+load_dotenv()
+
+hostip = os.getenv("HOST_IP")
+apikey = os.getenv("API_KEY")
+apisecret = os.getenv("API_SECRET")
+
+PfsenseFauxapi = PfsenseFauxapi(hostip, apikey, apisecret)
+
+########## Script starts here ##########
+
+config = PfsenseFauxapi.config_get()
+
+response_data = {}
+for user in config['system']['user']:
+    response_data[user['name']] = user
+    del(response_data[user['name']]['name'])
+pprint.pprint(response_data)
