70ac4ee1c5feee0f7e3cf64996035fd9278199d8
Nasir Hafeez <nasirhafeez2002@gmail.com>
2020-12-09 09:32:38 +0500

    .

--- users-create.py
+++ users-create.py
@@ -4,15 +4,15 @@ import pprint
 UGMF = UserGroupManagementFauxapi(fauxapi_host, fauxapi_apikey, fauxapi_apisecret)
 
 # add_user
-user = UGMF.add_user('test1')
+user = UGMF.add_user(sys.argv[1])
 #print(json.dumps(user))
 pprint.pprint(user)
 
 # manage_user attributes
 attributes = {
     'disabled': False,
-    'password': 'test123',
+    'password': sys.argv[2],
 }
-user = UGMF.manage_user('test1', attributes)
+user = UGMF.manage_user(sys.argv[1], attributes)
 #print(json.dumps(user))
 pprint.pprint(user)
