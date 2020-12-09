0b15d1fac95d59e18aaf6c80c5bfa72e5b6e9cb4
Nasir Hafeez <nasirhafeez2002@gmail.com>
2020-12-09 12:17:07 +0500

    comments added

--- user_ovpn_remove.py
+++ user_ovpn_remove.py
@@ -5,13 +5,16 @@ UGMF = UserGroupManagementFauxapi(fauxapi_host, fauxapi_apikey, fauxapi_apisecre
 
 ######################### User Removal Script #########################
 #
-# This script will remove static IP binding of OpenVPN user
+# This script will remove static IP binding of OpenVPN user. It is
+# possible to have multiple entries for the same user in the OpenVPN
+# Client Specific Overrides. This script just removes the first one
+# amongst those entries.
 # 
-# It will be run with 1 command line arguments: username
+# It will be run with 1 command line argument: username
 #
 # python3 user_ovpn_remove.py username
 # 
 
-user = UGMF.remove_ovpn_csc('test5')
+user = UGMF.remove_ovpn_csc(sys.argv[1])
 
 pprint.pprint(user)
