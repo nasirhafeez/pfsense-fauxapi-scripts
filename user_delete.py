4d57014e4d69e8031ea3af7d5eccf2ed4925a646
Nasir Hafeez <nasirhafeez2002@gmail.com>
2020-12-09 09:37:45 +0500

    file rename

--- user_delete.py
+++ user_delete.py
@@ -2,5 +2,7 @@ from usergroup_management import *
 import pprint
 
 UGMF = UserGroupManagementFauxapi(fauxapi_host, fauxapi_apikey, fauxapi_apisecret)
-users = UGMF.get_users()
-pprint.pprint(users)
+
+user = UGMF.remove_user(sys.argv[1])
+
+pprint.pprint(user)
---
+++ user_get.py
@@ -0,0 +1,7 @@
+from usergroup_management import *
+import pprint
+
+UGMF = UserGroupManagementFauxapi(fauxapi_host, fauxapi_apikey, fauxapi_apisecret)
+
+users = UGMF.get_users()
+pprint.pprint(users)
