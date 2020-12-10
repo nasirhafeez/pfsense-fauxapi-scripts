0f3abbde24e8ece7fa3b669c45d84226f97429f0
Nasir Hafeez <nasirhafeez2002@gmail.com>
2020-12-10 18:32:55 +0500

    test

--- sg_test.py
+++ sg_test.py
@@ -1,3 +1,9 @@
-import subprocess
+#import subprocess
+#subprocess.call("php /root/sg_reconfig.php")
 
-subprocess.call("php /root/sg_reconfig.php")
+import pprint, sys
+from PfsenseFauxapi.PfsenseFauxapi import PfsenseFauxapi
+PfsenseFauxapi = PfsenseFauxapi('192.168.100.54', 'PFFAudv9Chfn2n7W1us5utjH', '9XjKvnBRdNDCviXr9bZsTzKkIl4rZwp06gs0ImW4vV6kE1k16ibJ4SyPLJcz')
+
+stats = PfsenseFauxapi.interface_stats('em0')
+pprint.pprint(stats) 
