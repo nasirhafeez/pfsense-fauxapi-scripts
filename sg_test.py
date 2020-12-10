36955bcf710818ea9c634f9d1d50fcf5c9ac5d24
Nasir Hafeez <nasirhafeez2002@gmail.com>
2020-12-10 18:33:55 +0500

    .

--- sg_test.py
+++ sg_test.py
@@ -3,7 +3,7 @@
 
 import pprint, sys
 from PfsenseFauxapi.PfsenseFauxapi import PfsenseFauxapi
-PfsenseFauxapi = PfsenseFauxapi('192.168.100.54', 'PFFAudv9Chfn2n7W1us5utjH', '9XjKvnBRdNDCviXr9bZsTzKkIl4rZwp06gs0ImW4vV6kE1k16ibJ4SyPLJcz')
+PfsenseFauxapi = PfsenseFauxapi('18.217.115.232', 'PFFAudv9Chfn2n7W1us5utjH', '9XjKvnBRdNDCviXr9bZsTzKkIl4rZwp06gs0ImW4vV6kE1k16ibJ4SyPLJcz')
 
 stats = PfsenseFauxapi.interface_stats('em0')
 pprint.pprint(stats) 
