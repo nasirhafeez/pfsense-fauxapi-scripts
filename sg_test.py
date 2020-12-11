8ea75c74d5940be0a8817d681c6c575c0d1cbcee
Nasir Hafeez <nasirhafeez2002@gmail.com>
2020-12-11 10:02:35 +0500

    test

--- sg_test.py
+++ sg_test.py
@@ -1,9 +1,19 @@
 #import subprocess
 #subprocess.call("php /root/sg_reconfig.php")
 
-import pprint, sys
+import pprint, sys, os
 from PfsenseFauxapi.PfsenseFauxapi import PfsenseFauxapi
-PfsenseFauxapi = PfsenseFauxapi('18.217.115.232', 'PFFAudv9Chfn2n7W1us5utjH', '9XjKvnBRdNDCviXr9bZsTzKkIl4rZwp06gs0ImW4vV6kE1k16ibJ4SyPLJcz')
+from dotenv import load_dotenv
+
+load_dotenv()
+
+hostip = os.getenv("HOST_IP")
+apikey = os.getenv("API_KEY")
+apisecret = os.getenv("API_SECRET")
+
+PfsenseFauxapi = PfsenseFauxapi(hostip, apikey, apisecret)
+
+########## Script starts here ##########
 
 stats = PfsenseFauxapi.interface_stats('em0')
-pprint.pprint(stats) 
+pprint.pprint(stats)
