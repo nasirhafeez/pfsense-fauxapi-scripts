02dd8a079f37f653e6c4e48176fd7594fe6006ed
Nasir Hafeez <nasirhafeez2002@gmail.com>
2020-12-29 14:33:22 +0500

    comments updated

--- sg_groupacl_setup.py
+++ sg_groupacl_setup.py
@@ -7,9 +7,9 @@ UGMF = UserGroupManagementFauxapi(fauxapi_host, fauxapi_apikey, fauxapi_apisecre
 #
 # This script will setup or modify a Group ACL in SquidGuard
 # 
-# It will be run with 2 command line arguments: group_acl_name source_ip
+# It will be run with 3 command line arguments: group_acl_name source_ip rules
 #
-# python3 add_sg_groupacl.py group_acl_name source_ip rules
+# python3 add_sg_groupacl.py group_acl_name source_ip 'whitelist !blacklist !all [ all]'
 # 
 
 user = UGMF.add_sg_groupacl(sys.argv[1], sys.argv[2], sys.argv[3])
