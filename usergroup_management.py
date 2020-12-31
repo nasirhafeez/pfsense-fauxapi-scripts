8ec815566e48b9ffe5e07d7fbb8f4be06e57bdcc
Nasir Hafeez <nasirhafeez2002@gmail.com>
2020-12-31 15:57:01 +0500

    debug

--- usergroup_management.py
+++ usergroup_management.py
@@ -476,7 +476,7 @@ class UserGroupManagementFauxapi():
         self._reload_system_config()
 
         for c_index, c in enumerate(self.system_config['cron']['item']):
-            if c['who'] == 'clamav' AND c['command'] == '/usr/local/bin/freshclam --config-file=/usr/local/etc/freshclam.conf':
+            if ((c['who'] == 'clamav') AND (c['command'] == '/usr/local/bin/freshclam --config-file=/usr/local/etc/freshclam.conf')):
                 print('cron index: ', c_index)
                 print(c)
         # else:
